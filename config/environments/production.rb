b5456dc01d315f2e2359ed3ed7539ccdebdf18e5
cagum307 <cagum307@yahoo.co.jp>
2020-08-28 13:18:04 +0000

    modify_inquiry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,8 @@ Rails.application.configure do
   # Setup the mailer config
   # Use SendGrid - Add-ons - Heroku
   config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.default_url_options = { host: 'herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'chls.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
