3765768de863088e84b8190cb29318479d99a9a4
cagum307 <cagum307@yahoo.co.jp>
2020-08-28 11:29:42 +0000

    about

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'abouts/index'
   get 'toppages/index'
   root to: 'toppages#index'
+  
+  get 'abouts', to: 'abouts#index'
   
   get 'lesson01s', to: 'lesson01s#index'
   get 'lesson02s', to: 'lesson02s#index'
